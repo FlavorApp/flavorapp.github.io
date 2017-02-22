00ff5b4f33817d121d442d1a29a6a09e690b5c90
Flavor <flavor.lv@gmail.com>
2017-02-22 11:47:42 -0800

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -14,8 +14,7 @@ function signIn() {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
 	  var token = result.credential.accessToken;
 	  // The signed-in user info.
-	  user = result.user;
-	  showWelcomeContainer();
+	  var user = result.user;
 	  // ...
 	}).catch(function(error) {
 	  // Handle Errors here.
@@ -26,7 +25,7 @@ function signIn() {
 	  // The firebase.auth.AuthCredential type that was used.
 	  var credential = error.credential;
 	  // ...
-	});
+});
 
 };
 
