f47e13da485970163b7d03711a9f4537bf0b7faa
Flavor <flavor.lv@gmail.com>
2017-03-03 10:55:38 -0800

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -63,7 +63,7 @@ function confirmUpload() {
 			'caption': $("#imgDesc").val()
 		},
 	};
-	var uploadTask = firebase.storage().ref().child('dogImages/' + selectedFile.name).put(selectedFile, metadata);
+	var uploadTask = firebase.storage().ref().child('FoodImages/' + selectedFile.name).put(selectedFile, metadata);
 	// Register three observers:
 	// 1. 'state_changed' observer, called any time the state changes
 	// 2. Error observer, called on failure
